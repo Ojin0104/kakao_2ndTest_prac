dbcd8b8a3c1c1426e3194433f8ff2c75695709b7
yeongjin han <hanyj0104@naver.com>
2023-05-09 15:35:53 +0900

    feat: add API CONSTANT

---
+++ src/main/java/ConstantString.java
@@ -0,0 +1,14 @@
+public class ConstantString {
+    public static String BASE_URL="https://kox947ka1a.execute-api.ap-northeast-2.amazonaws.com/prod/users";
+
+    public static String AUTH_TOKEN="523673768fbb94484963968d3f56070a";
+
+    public static String START_API="/start";
+
+    public static String LOCATIONS_API="/locations";
+
+    public static String TRUCKS_API="/trucks";
+
+    public static String SIMULATE_API="/simulate";
+}
+
