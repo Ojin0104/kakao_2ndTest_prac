841052d2148569698ed4b7c026a15991acc2ee8b
yeongjin <hanyj0104@naver.com>
2023-05-09 19:47:48 +0900

    feat: Create Command Class

---
+++ src/main/java/Command.java
@@ -0,0 +1,30 @@
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+
+public class Command {
+
+    private int truck_id;
+    private ArrayList<Integer> command;
+
+    public Command(int truck_id,ArrayList<Integer> commands){
+        this.truck_id=truck_id;
+        this.command=commands;
+    }
+
+    public int getTruck_id() {
+        return truck_id;
+    }
+
+    public void setTruck_id(int truck_id) {
+        this.truck_id = truck_id;
+    }
+
+    public ArrayList<Integer> getCommand() {
+        return command;
+    }
+
+    public void setCommand(ArrayList<Integer> command) {
+        this.command = command;
+    }
+}
