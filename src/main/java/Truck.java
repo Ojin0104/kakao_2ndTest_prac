b8a41d6ae2dfa334c216378ec62dc416e2b45658
yeongjin <hanyj0104@naver.com>
2023-05-09 19:43:22 +0900

    feat: Create Truck Class

---
+++ src/main/java/Truck.java
@@ -0,0 +1,30 @@
+public class Truck {
+
+    private int id;
+    private int location_id;
+    private int loaded_bikes_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLocation_id() {
+        return location_id;
+    }
+
+    public void setLocation_id(int location_id) {
+        this.location_id = location_id;
+    }
+
+    public int getLoaded_bikes_id() {
+        return loaded_bikes_id;
+    }
+
+    public void setLoaded_bikes_id(int loaded_bikes_id) {
+        this.loaded_bikes_id = loaded_bikes_id;
+    }
+}
